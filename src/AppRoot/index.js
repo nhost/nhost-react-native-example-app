6fc0f552e5edf4ce583ba33491ff7599657368cc
Karthik-B-06 <karthik06051996@gmail.com>
2020-06-22 14:10:14 +0530

    1. Added axios and nhost-js-sdk

--- src/AppRoot/index.js
+++ src/AppRoot/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
+import { auth } from '../helpers/nhostSdk';
 
 const AppRoot = (props) => {
+  useEffect(() => {
+    // auth.login('karthik@timeless.co', '1234');
+  }, [])
   return (
     <View style={{ height: '100%', alignContent: 'center', alignItems: 'center', justifyContent: 'center' }}>
       <Text style={{ fontSize: 20 }}>Hello, World !</Text>
--- src/helpers/nhostSdk/index.js
+++ src/helpers/nhostSdk/index.js
@@ -1,8 +1,9 @@
 import nhost from 'nhost-js-sdk';
 import { BACKEND_ENDPOINT } from '../api';
-
+import AsyncStorage from '@react-native-community/async-storage';
 const config = {
   endpoint: BACKEND_ENDPOINT,
+  storage: AsyncStorage
 };
 
 nhost.initializeApp(config);
@@ -10,4 +11,7 @@ nhost.initializeApp(config);
 const auth = nhost.auth();
 const storage = nhost.storage();
 
-export { auth, storage };
+export {
+  auth,
+  storage
+};
